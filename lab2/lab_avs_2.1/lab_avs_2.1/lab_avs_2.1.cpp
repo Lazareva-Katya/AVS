e6bcdfa7b2cc3f8a3e68a1a1106801eb5737e362
Lazareva-Katya <lazareva.katya.22@gmail.com>
2020-12-27 01:00:11 +0300

    Add two tasks

---
+++ lab2/lab_avs_2.1/lab_avs_2.1/lab_avs_2.1.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <atomic>
+
+using namespace std;
+
+const int NumTasks = 1024 * 1024;
+int counter1 = 0;
+atomic <unsigned int> counter2 = 0;
+mutex m;
+
+void mutex_func(int8_t* arr)
+{
+	int counter = 0;
+	while (counter < NumTasks)
+	{
+		m.lock();
+		counter = counter1++;
+		arr[counter]++;
+		m.unlock();
+	}
+}
+
+void atomic_func(int8_t* arr)
+{
+	int counter = 0;
+	while (counter < NumTasks)
+	{
+		counter = counter2++;
+		arr[counter]++;
+	}
+}
+
+void mutex_sleep(int8_t* arr)
+{
+	int counter = 0;
+	while (counter < NumTasks)
+	{
+		m.lock();
+		counter = counter1++;
+		arr[counter]++;
+		this_thread::sleep_for(chrono::nanoseconds(10));
+		m.unlock();
+	}
+}
+
+void atomic_sleep(int8_t* arr)
+{
+	int counter = 0;
+	while (counter < NumTasks)
+	{
+		counter = counter2++;
+		arr[counter]++;
+		this_thread::sleep_for(chrono::nanoseconds(10));
+	}
+}
+
+void show(int NumThreads, float duration)
+{
+	cout << "streams: " << NumThreads << " time: " << duration << "\n\n";
+}
+
+void action(thread* arr_t, int8_t* arr, unsigned int NumThreads, bool flag, bool sleep)
+{
+	if (flag == true && sleep == false)
+	{
+		cout << "mutex:   ";
+	}
+	else if (flag == false && sleep == false)
+	{
+		cout << "atomic:   ";
+	}
+	else if (flag == true && sleep == true)
+	{
+		cout << "mutex sleep:   ";
+	}
+	else
+		cout << "atomic sleep:   ";
+
+	for (unsigned int i = 0; i < NumThreads; i++)
+	{
+		if (flag == true && sleep == false)
+		{
+			arr_t[i] = thread(mutex_func, arr);
+		}
+		else if (flag == false && sleep == false)
+		{
+			arr_t[i] = thread(atomic_func, arr);
+		}
+		else if (flag == true && sleep == true)
+		{
+			arr_t[i] = thread(mutex_sleep, arr);
+		}
+		else
+			arr_t[i] = thread(atomic_sleep, arr);
+		arr_t[i].join();
+	}
+}
+
+void act(int8_t* arr, thread* arr_t, unsigned int NumThreads, int switcher)
+{
+	chrono::high_resolution_clock::time_point first;
+	chrono::high_resolution_clock::time_point second;
+	chrono::duration<float> duration;
+	first = chrono::high_resolution_clock::now();
+	switch (switcher)
+	{
+	case 0: action(arr_t, arr, NumThreads, true, false);
+		break;
+	case 1: action(arr_t, arr, NumThreads, false, false);
+		break;
+	case 2: action(arr_t, arr, NumThreads, true, true);
+		break;
+	case 3: action(arr_t, arr, NumThreads, false, true);
+	default:
+		break;
+	}
+	second = chrono::high_resolution_clock::now();
+	duration = second - first;
+	show(NumThreads, duration.count());
+	counter1 = 0;
+	counter2 = 0;
+}
+
+int8_t activity(int8_t* arr, unsigned int NumThreads)
+{
+	thread* arr_t = new thread[NumThreads];
+	act(arr, arr_t, NumThreads, 0);
+	act(arr, arr_t, NumThreads, 1);
+	act(arr, arr_t, NumThreads, 2);
+	act(arr, arr_t, NumThreads, 3);
+	return *arr;
+}
+
+int main()
+{
+	int NumTreads[] = { 4, 8, 16, 32 };
+	int8_t* arr = new int8_t[NumTasks];
+
+	for (int i = 0; i < 4; i++)
+		activity(arr, NumTreads[i]);
+}
---
+++ lab2/lab_avs_2.2/lab_avs_2.2/lab_avs_2.2.cpp
@@ -0,0 +1,187 @@
+﻿#include <iostream>
+#include <mutex>
+#include <atomic>
+#include <queue>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+static const int taskNum = 4 * 1024 * 1024;
+static const int producerNum = 1; 
+static const int consumerNum = 1; 
+static const int queueSize = 4;
+atomic<int> sum(0);
+atomic<int> counter(0);
+atomic<int> sum2(0);
+atomic<int> counter2(0);
+vector<thread> prods;
+vector<thread> consus;
+vector<thread> prods2;
+vector<thread> consus2;
+uint8_t val;
+uint8_t val2;
+
+class DynamicQueue {
+private:
+    queue<uint8_t> q;
+    mutex m;
+
+public:
+    void push(uint8_t val);
+    bool pop(uint8_t& val);
+} q;
+
+void DynamicQueue::push(uint8_t val) {
+    lock_guard<mutex> lk(m);
+    q.push(val);
+}
+
+bool DynamicQueue::pop(uint8_t& val) {
+    m.lock();
+
+    if (q.empty()) {
+        m.unlock();
+        this_thread::sleep_for(chrono::milliseconds(1));
+        m.lock();
+        if (q.empty())
+        {
+            m.unlock();
+            return false;
+        }
+    }
+
+    val = q.front();
+    q.pop();
+    m.unlock();
+    return true;
+
+}
+
+class SecondQueue {
+private:
+    queue<uint8_t> q2;
+    mutex m2;
+    condition_variable var_push;
+    condition_variable var_pop;
+    bool full() {
+        if (q2.size() >= queueSize)
+            return true;
+        else
+            return false;
+    }
+
+public:
+    void push(uint8_t val);
+    bool pop(uint8_t& val);
+} q2;
+
+void SecondQueue::push(uint8_t val) {
+    unique_lock<mutex> lk(m2);
+
+    var_push.wait(lk, [this] {return !full(); });
+    q2.push(val);
+
+    var_pop.notify_all();
+}
+
+bool SecondQueue::pop(uint8_t& val) {
+    unique_lock<mutex> lk(m2);
+    if (q2.empty()) {
+        var_pop.wait_for(lk, chrono::milliseconds(1));
+        if (q2.empty())
+            return false;
+    }
+    val = q2.front();
+    q2.pop();
+    var_push.notify_all();
+    return true;
+}
+
+void cons() {
+    int s = 0;
+    val = 0;
+    int c = 0;
+
+    while (q.pop(val)) {
+        c = counter;
+        s += val;
+        counter++;
+
+        if (c >= taskNum * producerNum) {
+            break;
+        }
+    }
+
+    sum.fetch_add(s);
+}
+
+void prod() {
+    for (int i = 0; i < taskNum; i++) {
+        q.push(1);
+    }
+}
+
+void prod2() {
+    for (int i = 0; i < taskNum; i++) {
+        q2.push(1);
+    }
+}
+
+void show()
+{
+    cout << "sum1: " << sum << endl;
+    cout << "sum2: " << sum2 << endl;
+    cout << endl;
+}
+
+void cons2() {
+    int s = 0;
+    val2 = 0;
+    int c = 0;
+
+    while (q2.pop(val2)) {
+        c = counter2;
+        s += val2;
+        counter2++;
+
+        if (c >= taskNum * producerNum) {
+            break;
+        }
+    }
+    sum2.fetch_add(s);
+}
+
+void first_queue()
+{
+    for (int i = 0; i < producerNum; i++) {
+        prods.push_back(thread(prod));
+        prods[i].join();
+    }
+
+    for (int i = 0; i < consumerNum; i++) {
+        consus.push_back(thread(cons));
+        consus[i].join();
+    }
+}
+
+int main() {
+    first_queue();
+    for (int i = 0; i < producerNum; i++) {
+        prods2.push_back(thread(prod2));
+    }
+
+    for (int i = 0; i < consumerNum; i++) {
+        consus2.push_back(thread(cons2));
+    }
+
+    for (int i = 0; i < producerNum; i++) {
+        prods2[i].join();
+    }
+
+    for (int i = 0; i < consumerNum; i++) {
+        consus2[i].join();
+    }
+    show();
+    return 0;
+}
